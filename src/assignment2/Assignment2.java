b36009dfc3bcf8a664a42f78d86e118293594ac4
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-09-14 10:11:14 -0400

    School Sept.13

--- src/assignment2/Assignment2.java
+++ src/assignment2/Assignment2.java
@@ -10,10 +10,40 @@ package assignment2;
  */
 public class Assignment2 {
 
+    public int digitSum(int n) {
+
+        if (n < 10) {
+            return (n / 10 + n % 10);
+        } else { //if(n >=10&& n<100){
+            return digitSum(n / 10 + n % 10);
+        }
+        // return 0; 
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Assignment2 test = new Assignment2();
+        System.out.println("----0-9");
+        for (int i = 0; i < 10; i++) {
+            int A2Q1 = test.digitSum(i);
+            System.out.println(A2Q1);
+        }
+        System.out.println("---10-19");
+        for (int i = 10; i < 20; i++) {
+            int A2Q1 = test.digitSum(i);
+            System.out.println(A2Q1);
+        }
+        System.out.println("---20-29");
+        for (int i = 20; i < 30; i++) {
+            int A2Q1 = test.digitSum(i);
+            System.out.println(A2Q1);
+        }
+        System.out.println("SELF TEST");
+        int A2Q1 = test.digitSum(156);
+        System.out.println(A2Q1);
+            
+                
     }
 }
