854c9851a890751d4f9cc0237f168e6e8596a61b
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-09-14 10:11:45 -0400

    School Sept.14

--- src/assignment2/Assignment2.java
+++ src/assignment2/Assignment2.java
@@ -13,9 +13,9 @@ public class Assignment2 {
     public int digitSum(int n) {
 
         if (n < 10) {
-            return (n / 10 + n % 10);
+            return n;
         } else { //if(n >=10&& n<100){
-            return digitSum(n / 10 + n % 10);
+            
         }
         // return 0; 
     }
