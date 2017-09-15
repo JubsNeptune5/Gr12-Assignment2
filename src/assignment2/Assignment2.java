635d554e03ed7081fe7b3c65d25e4674b4f135de
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-09-15 10:13:40 -0400

    School Sept.15

--- src/assignment2/Assignment2.java
+++ src/assignment2/Assignment2.java
@@ -10,16 +10,44 @@ package assignment2;
  */
 public class Assignment2 {
 
-    public int digitSum(int n) {
+    /**
+     * Method to find the sum of all the digits of a integer
+     *
+     * @param n
+     * @param sum
+     * @return
+     */
+    public int digitSum(int n, int sum) {
 
+        //check if the number has only one digit
         if (n < 10) {
-            return n;
-        } else { //if(n >=10&& n<100){
-           int  d = n%10;
-           
-           
+            //have the one and only digit added to the sum to produce an answer
+            sum = n + sum;
+            return sum;
+        } else {
+            //fint the remainder of the number divided by ten to show you the last digit of a number
+            int d = n % 10;
+            //Add the last digit to the sum od the digits
+            sum = sum + d;
+            //Return the number divided by 10 to lose a digit place, and the sum of the digits
+            return digitSum(n / 10, sum);
         }
         // return 0; 
+    }
+
+    public int digitRoot(int n) {
+        int x = digitSum(n, 0);
+        //x=x/10;
+        if (x < 10) {
+           // x=x*10;
+            System.out.println("x<10");
+            System.out.println(x);
+            return x;
+        } else {
+            System.out.println("x");
+            System.out.println(x);
+            return digitRoot(x);
+        }
     }
 
     /**
@@ -27,25 +55,16 @@ public class Assignment2 {
      */
     public static void main(String[] args) {
         Assignment2 test = new Assignment2();
-        System.out.println("----0-9");
-        for (int i = 0; i < 10; i++) {
-            int A2Q1 = test.digitSum(i);
-            System.out.println(A2Q1);
-        }
-        System.out.println("---10-19");
-        for (int i = 10; i < 20; i++) {
-            int A2Q1 = test.digitSum(i);
-            System.out.println(A2Q1);
-        }
-        System.out.println("---20-29");
-        for (int i = 20; i < 30; i++) {
-            int A2Q1 = test.digitSum(i);
-            System.out.println(A2Q1);
-        }
-        System.out.println("SELF TEST");
-        int A2Q1 = test.digitSum(156);
+        int n = 999999999;
+        //Test A2Q1
+        System.out.println("------A2Q1 TEST-------");
+        int A2Q1 = test.digitSum(n, 0);
         System.out.println(A2Q1);
-            
-                
+        //Test A2Q2
+        System.out.println("------A2Q2 TEST-------");
+        int A2Q2 = test.digitRoot(n);
+        System.out.println(A2Q2);
+
+
     }
 }
